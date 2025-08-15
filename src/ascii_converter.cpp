2d47b8fb69a14c4d5eea86448d322e4087aa3f1a
Ansh Swaroop <anshswaroop14@gmail.com>
2025-08-15 22:16:05 +0530

    Implemented image conversion logic

--- src/ascii_converter.cpp
+++ src/ascii_converter.cpp
@@ -1,5 +1,8 @@
 #include "ascii_converter.hpp"
 
+/// @brief ASCII character set used for conversion in decresing order of brightness since background of the terminal will be black
+const std::string ascii_chars =  "@$B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.";
+
 char mapToAscii(const uchar& pixel, const std::vector<char>& ascii_chars) {
     int index = static_cast<int>(pixel * (ascii_chars.size() - 1) / 255);
     return ascii_chars[index];
@@ -12,4 +15,28 @@ uchar getCombinedPixel(const cv::Mat& image, int x, int y) {
         return 0;
     }
     return (image.at<uchar>(y1, x) + image.at<uchar>(y2, x)) / 2;
+}
+
+void toGrayscale(cv::Mat &image) {
+    if (image.channels() == 3) {
+        cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
+    } else if (image.channels() == 4) {
+        cv::cvtColor(image, image, cv::COLOR_BGRA2GRAY);
+    }
+}
+
+void convert_image(cv::Mat &image ,std::string &output){
+    if (image.empty()) {
+        output = "Image is empty!";
+        return;
+    }
+    
+    output.clear();
+    for (int y = 0; y < image.rows; y += 2) {
+        for (int x = 0; x < image.cols; x++) {
+            uchar pixel = getCombinedPixel(image, x, y);
+            output += mapToAscii(pixel, ascii_chars);
+        }
+        output += '\n';
+    }
 }
